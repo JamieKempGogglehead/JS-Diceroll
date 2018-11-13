ce5581883036d19178f3e724a0cbde7185416c6c
unknown <jamie@gogglehead.com>
2018-11-13 17:07:58 +0100

    initial commit

---
+++ Fibonacci Question and Receive.js
@@ -0,0 +1,33 @@
+var Start=0;
+0
+
+var First=1;
+1
+
+var Second=First+Start;
+1
+
+var Third=Second+First;
+2
+
+var Fourth=Third+Second;
+3
+
+var Fifth=Fourth+Third;
+5
+
+var Sixth=Fifth+Fourth;
+8
+
+var Seventh=Sixth+Fifth;
+13
+
+var Eighth=Seventh+Sixth;
+21
+
+var Ninth=Eighth+Seventh;
+34
+
+var Tenth=Ninth+Eighth;
+
+
---
+++ Fibonacci.js
@@ -0,0 +1,48 @@
+var Start=0;
+0
+console.log(Start);
+var First=1;
+1
+console.log(First);
+var Second=First+Start;
+1
+console.log(Second);
+var Third=Second+First;
+2
+console.log(Third);
+var Fourth=Third+Second;
+3
+console.log(Fourth);
+var Fifth=Fourth+Third;
+5
+console.log(Fifth);
+var Sixth=Fifth+Fourth;
+8
+console.log(Sixth);
+var Seventh=Sixth+Fifth;
+13
+console.log(Seventh);
+var Eighth=Seventh+Sixth;
+21
+console.log(Eighth);
+var Ninth=Eighth+Seventh;
+34
+console.log(Ninth);
+Tenth=Ninth+Eighth
+55
+console.log(Tenth)
+Eleventh=Tenth+Ninth
+89
+console.log(Eleventh)
+Twelfth=Eleventh+Tenth
+144
+console.log(Twelfth)
+Thirteenth=Twelfth+Eleventh
+233
+console.log(Thirteenth)
+Fourteenth=Thirteenth+Twelvfth
+377
+console.log(Fourteenth)
+Fifthteenth=Fourteenth+Thirteenth
+610
+console.log(Fifthteenth)
