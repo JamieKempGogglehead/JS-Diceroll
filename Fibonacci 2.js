69237de3e7363a0ee405e201753a254921828a4d
JamieKempGogglehead <45009564+JamieKempGogglehead@users.noreply.github.com>
2018-11-14 12:21:17 +0000

    Add files via upload

--- Fibonacci 2.js
+++ Fibonacci 2.js
@@ -1,32 +1,35 @@
 var Start=0;
 0
-
 var First=1;
 1
-
 var Second=First+Start;
 1
-
 var Third=Second+First;
 2
-
 var Fourth=Third+Second;
 3
-
 var Fifth=Fourth+Third;
 5
-
 var Sixth=Fifth+Fourth;
 8
-
 var Seventh=Sixth+Fifth;
 13
-
 var Eighth=Seventh+Sixth;
 21
-
 var Ninth=Eighth+Seventh;
 34
+var Tenth=Ninth+Eighth
+55
+var Eleventh=Tenth+Ninth
+89
+var Twelfth=Eleventh+Tenth
+144
+var Thirteenth=Twelfth+Eleventh
+233
+var Fourteenth=Thirteenth+Twelvfth
+377
+var Fifthteenth=Fourteenth+Thirteenth
+610
 
 
 
--- HighRoller.js
+++ HighRoller.js
@@ -1,12 +1,23 @@
 var die = {
     size:6,
-    totalRolls: 0,
     roll: function(dieSize) {
         var result = Math.ceil(this.size * Math.random());
-        this.totalRolls += 1;
         return result;
     }
 };
+
+var totalResult=0;
+
 die.size = process.argv[2];
-console.log(die.roll());
-console.log("The die was rolled (" + die.totalRolls + ") time(s)");
+
+for (totalRolls= 0; (totalRolls< process.argv[3]); totalRolls++) {
+   var result=Math.ceil(die.size * Math.random());
+   console.log(result) 
+   totalResult+=result
+}
+
+var average=totalResult/totalRolls;
+
+console.log("The die was rolled (" + totalRolls + ") time(s)");
+console.log("You rolled a total of ("+ totalResult+ ")" );
+console.log("You rolled an average of("+average+")");
